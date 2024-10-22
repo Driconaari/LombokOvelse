14179249bb4359ba2fd4af87f9fca63da9ddecd6
asger <asgervb@gmail.com>
2024-10-22 14:12:42 +0200

    ændrede cklasserne

--- src/main/java/web/LombokOvelse/repository/StudentRepository.java
+++ src/main/java/web/LombokOvelse/repository/StudentRepository.java
@@ -1,4 +1,9 @@
 package web.LombokOvelse.repository;
 
-public interface StudentRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import web.LombokOvelse.model.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
 }
