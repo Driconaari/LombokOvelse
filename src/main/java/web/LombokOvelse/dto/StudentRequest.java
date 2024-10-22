a5874a514ab3e24041245978308a9ee2c7d4245e
asger <asgervb@gmail.com>
2024-10-22 14:47:59 +0200

    name change

--- src/main/java/web/LombokOvelse/dto/StudentRequest.java
+++
@@ -1,8 +0,0 @@
-package web.LombokOvelse.dto;
-
-
-import java.time.LocalDate;
-import java.time.LocalTime;
-
-public record StudentRequest(String name, String password, LocalDate birthDate, LocalTime bornTime) {
-}
